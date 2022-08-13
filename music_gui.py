37520f9e2907a1344686117bf25739c99b01c7fa
Francisco R Castro <iceman09@hotmail.com>
2022-08-12 22:16:11 -0500

    change made to logger to add time fo error to log file

--- music_gui.py
+++ music_gui.py
@@ -25,8 +25,8 @@ f_handler = logging.FileHandler('errors.log')
 f_handler.setLevel(logging.ERROR)
 
 #Formatters
-c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 # c_handler.setFormatter(c_format)
 f_handler.setFormatter(f_format)
 #Handlers
